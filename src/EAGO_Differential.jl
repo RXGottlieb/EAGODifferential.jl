912f81de0bc53125378e7c324862b04c0842b483
Robert Gottlieb <Robert.x.gottlieb@uconn.edu>
2021-08-17 10:04:21 -0400

    Conversions to work locally

--- src/EAGO_Differential.jl
+++ src/EAGO_Differential.jl
@@ -1,8 +1,18 @@
+
+# add EAGO#0.3.1
+# rm MINLPLib
+# rm StaticArrays
+# Opposite:
+# ]add EAGO#master
+# Pkg.add(PackageSpec(path="C:\\Users\\Robert\\OneDrive\\Documents\\Github Repositories\\MINLPLibJuMP.jl"))
+# ]add StaticArrays
+
 module EAGO_Differential
 
 
     using EAGO, MathOptInterface, LinearAlgebra, DataFrames, CSV
 
+    const IntervalType = Interval{Float64}
     const MOI = MathOptInterface
     const export_path  = "C:/Users/wilhe/Dropbox/Apps/Overleaf/Global optimization with stiff ODE constraints/Plotting_Code_Data"
 
@@ -10,16 +20,16 @@ module EAGO_Differential
                  num_state_variables, num_decision_variables,
                  eval_constraint_cc, eval_constraint_cc_grad
 
-    include("src/calc_ode.jl")
+    include("C:/Users/Robert/OneDrive/Documents/Github Repositories/EAGODifferential.jl/src/src/calc_ode.jl")
+
+    include("C:/Users/Robert/OneDrive/Documents/Github Repositories/EAGODifferential.jl/src/src/lower_evaluator/lower_evaluator.jl")
+    include("C:/Users/Robert/OneDrive/Documents/Github Repositories/EAGODifferential.jl/src/src/upper_evaluator/upper_evaluator.jl")
+    include("C:/Users/Robert/OneDrive/Documents/Github Repositories/EAGODifferential.jl/src/src/solve_ode.jl")
+    include("C:/Users/Robert/OneDrive/Documents/Github Repositories/EAGODifferential.jl/src/src/data_handling.jl")
 
     export ImplicitODELowerEvaluator, ImplicitODEUpperEvaluator,
            build_evaluator!, set_current_node!, set_last_node!,
            num_state_variables, num_decision_variables, solve_ode,
            export_path, eval_constraint_cc, eval_constraint_cc_grad
-
-    include("src/lower_evaluator/lower_evaluator.jl")
-    include("src/upper_evaluator/upper_evaluator.jl")
-    include("src/solve_ode.jl")
-    include("src/data_handling.jl")
 
 end # module
